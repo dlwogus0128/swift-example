9cd4ef7fb4c14054ca6cf2acbbad24253c39cc75
JaeHyun Lee <dlwogus0128@ajou.ac.kr>
2024-02-06 16:33:12 +0900

    [Feat] HostingController 사용하기

---
+++ swiftui-example/HostingControllerDemo/HostingControllerDemo/SwiftUIView.swift
@@ -0,0 +1,28 @@
+//
+//  SwiftUIView.swift
+//  HostingControllerDemo
+//
+//  Created by 픽셀로 on 2/6/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    
+    var text: String
+    
+    var body: some View {
+        VStack {
+            Text(text)
+            HStack {
+                Image(systemName: "smiley")
+                Text("This is a SwiftUI View")
+            }
+        }
+        .font(.largeTitle)
+    }
+}
+
+//#Preview {
+//    SwiftUIView(text: "Sample Text")
+//}
